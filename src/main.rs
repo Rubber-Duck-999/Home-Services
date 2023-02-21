55bb52b77ed646a732d9686d649d77f1e725ffb1
Rubberduck <simon.crowther95@hotmail.co.uk>
2023-02-21 08:15:24 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,23 @@ extern crate simple_logger;
 
 use log::Level;
 
-fn main() {
-    simple_logger::init_with_level(Level::Trace).unwrap();
+use std::error::Error;
+use std::time::Duration;
+use std::{mem, thread};
 
-    if log_enabled!(Level::Trace) {
-        info!("Logging has been enabled to trace");
+use blinkt::Blinkt;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut blinkt = Blinkt::new()?;
+    let (red, green, blue) = (&mut 255, &mut 0, &mut 0);
+
+    loop {
+        blinkt.set_all_pixels(*red, *green, *blue);
+        blinkt.show()?;
+
+        thread::sleep(Duration::from_millis(250));
+
+        mem::swap(red, green);
+        mem::swap(red, blue);
     }
-
-    process::exit(0);
 }
